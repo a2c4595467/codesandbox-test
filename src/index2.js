55ba49c2ff67a302f9531747047676e438fcb6f5
a2cNSMR <a2c4595467@hotmail.com>
2021-12-03 19:52:37 +0900

    モダンJSの勉強 map, filter の勉強。

--- src/index2.js
+++ src/index2.js
@@ -32,3 +32,32 @@ console.log(arr5);
 const arr6 = [30, 40];
 const arr7 = [...arr4, ...arr6];
 console.log(arr7);
+
+/**
+ * map や filter を使った配列の処理
+ */
+const nameArr = ["ヤマダ", "山本", "田中"];
+// 従来の書き方
+console.log("従来の配列ループ");
+for (let index = 0; index < nameArr.length; index++) {
+  console.log(nameArr[index]);
+}
+
+// map
+console.log("mapでの配列ループ");
+const nameArr2 = nameArr.map((name) => {
+  return name;
+});
+console.log(nameArr2);
+nameArr.map((name) => console.log(name));
+nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// ----------------------------
+// filter
+// ----------------------------
+const numArr = [1, 2, 3, 4, 5];
+console.log("奇数のみ取り出す");
+const newNumArr = numArr.filter((num) => {
+  return num % 2 === 1;
+});
+console.log(newNumArr);
